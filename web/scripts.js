d6e8b72750460642c73fc5c2c04ecf91249b7311
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2012-03-22 22:38:37 -0400

    Added new flowers

--- web/scripts.js
+++ web/scripts.js
@@ -19,7 +19,20 @@ $(function() {
   
 	var overlay = new DrawOverlay(map,{
 		'brushes' : {
-			'flower' : ['flowers/red.png','flowers/blue.png','flowers/pink.png']
+			'flower' : [
+				'flowers/1.png',
+				'flowers/2.png',
+				'flowers/3.png',
+				'flowers/4.png',
+				'flowers/5.png',
+				'flowers/6.png',
+				'flowers/7.png',
+				'flowers/8.png',
+				'flowers/9.png',
+				'flowers/10.png',
+				'flowers/11.png',
+				'flowers/12.png'
+			]
 		}
 	}); 
 	
